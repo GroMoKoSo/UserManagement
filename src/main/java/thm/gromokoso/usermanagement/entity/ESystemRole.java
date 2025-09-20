aaeed6f3c43b9a542ceccca1bd5d71fcb84974f2
Jannik Heimann <jannik.heimann@zdh.thm.de>
2025-09-20 16:59:33 +0200

    feat: enhanced Enums to allow string representation in http request
    #7

--- src/main/java/thm/gromokoso/usermanagement/entity/ESystemRole.java
+++ src/main/java/thm/gromokoso/usermanagement/entity/ESystemRole.java
@@ -1,6 +1,30 @@
 package thm.gromokoso.usermanagement.entity;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum ESystemRole {
-    MEMBER,
-    ADMIN
+    MEMBER("Member"),
+    ADMIN("Admin");
+
+    private final String value;
+
+    ESystemRole(String value) {
+        this.value = value;
+    }
+
+    @JsonValue   // Jackson verwendet diesen Wert bei (De-)Serialisierung
+    public String getValue() {
+        return value;
+    }
+
+    @JsonCreator // Jackson mappt String -> Enum
+    public static ESystemRole fromValue(String value) {
+        for (ESystemRole role : values()) {
+            if (role.value.equalsIgnoreCase(value)) {
+                return role;
+            }
+        }
+        throw new IllegalArgumentException("Unknown role: " + value);
+    }
 }

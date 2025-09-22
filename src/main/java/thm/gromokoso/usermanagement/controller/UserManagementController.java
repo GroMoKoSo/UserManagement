1a72bd2ecb96decc361daa0220810fd87499c7c3
Jannik Heimann <jannik.heimann@zdh.thm.de>
2025-09-22 12:21:23 +0200

    fix: add missing Tag
    #3

--- src/main/java/thm/gromokoso/usermanagement/controller/UserManagementController.java
+++ src/main/java/thm/gromokoso/usermanagement/controller/UserManagementController.java
@@ -5,11 +5,13 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 import thm.gromokoso.usermanagement.dto.*;
 
 import java.util.List;
 
+@Tag(name = "USer Management", description = "All Endpoints related to manage users.")
 public interface UserManagementController {
 
     @ApiResponses(value = {

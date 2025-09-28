e71a0f9ac96373c3d2dc8e028502e316ca74253a
DerHighman <jannikheimann3@gmail.com>
2025-09-28 19:24:01 +0200

    fix: change Exception in Token provider
    #3

--- src/main/java/thm/gromokoso/usermanagement/security/TokenProvider.java
+++ src/main/java/thm/gromokoso/usermanagement/security/TokenProvider.java
@@ -4,24 +4,23 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Component;
-
-import javax.security.sasl.AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 
 @Component
 public class TokenProvider {
 
-    public String getToken() throws AuthenticationException {
+    public String getToken() throws OAuth2AuthenticationException {
         return getJWTToken().getTokenValue();
     }
 
-    public String getUsernameFromToken() throws AuthenticationException {
+    public String getUsernameFromToken() throws OAuth2AuthenticationException  {
         return getJWTToken().getClaim("preferred_username");
     }
 
-    private Jwt getJWTToken() throws AuthenticationException {
+    private Jwt getJWTToken() throws OAuth2AuthenticationException  {
         var token = SecurityContextHolder.getContext().getAuthentication();
         if (!(token instanceof JwtAuthenticationToken jwtToken)) {
-            throw new AuthenticationException("JWT token is not valid");
+            throw new OAuth2AuthenticationException ("JWT token is not valid");
         }
         return jwtToken.getToken();
     }

cdc9a51aac690cd7ab7732d38275fd0f1e14509a
DerHighman <jannikheimann3@gmail.com>
2025-09-29 17:34:18 +0200

    feat: Add cors allowed origins in application.yml
    Also added prod config
    
    #3

--- src/main/java/thm/gromokoso/usermanagement/config/WebConfig.java
+++ src/main/java/thm/gromokoso/usermanagement/config/WebConfig.java
@@ -1,17 +1,23 @@
 package thm.gromokoso.usermanagement.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+    private final String allowedOrigins;
+
+    public WebConfig(@Value("${}") String allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "http://localhost:5173",
-                        "http://localhost:8080"
+                        allowedOrigins.split(",")
                 )
                 .allowedMethods("GET","POST","PUT","PATCH","DELETE","OPTIONS")
                 .allowedHeaders("*")
